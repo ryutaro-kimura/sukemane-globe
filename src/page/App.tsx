bd57eb4b9c1a5cf3ff0578d325bd4ce924505fbe
upauparuru <118331316+upauparuru@users.noreply.github.com>
2022-12-10 22:57:09 +0900

    #1 feat:three.jsで地球儀の画像を表示する機能の追加

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { useLoader } from "@react-three/fiber";
+import * as THREE from 'three';
+import img from "./earthmap1k.jpg";
 
-function App() {
+const App = () => {
+  const texture = useLoader(THREE.TextureLoader, img);
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div id="canvas-container">
+      <Canvas
+        camera={{
+          position: [0, 0, 1000],
+          aspect: 960 / 540,
+        }
+        }>
+        <mesh>
+          <sphereGeometry args={[300]} />
+          <meshStandardMaterial map={texture} />
+        </mesh>
+        <directionalLight color="white" position={[1, 1, 1]} />
+      </Canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
