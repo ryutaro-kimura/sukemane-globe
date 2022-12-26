217eb70b8ad7efc9d8c916b6a7f8eda79ae19718
ryutaro-kimura <sasuker2264@gmail.com>
2022-12-26 15:05:12 +0900

    fix: loadingの形崩れ直した

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -18,13 +18,13 @@ export const Loading = () => {
   return (
     <>
       <div className="h-screen bg-zinc-900 flex flex-col justify-center items-center">
-        <div className="inline-block w-52 h-48 text-white relative mb-10">
-          <img src={pic} alt="globe" className="z-0 h-full w-full border-4 border-red-600" />
-          <p className="text-4xl absolute top-1/2 left-1/3 z-40">point</p>
-          <p className="z-30 absolute bottom-1">地球をぐるぐる回しながら世界で活躍するフェローを探せる！</p>
-          <div className="bg-red-600 absolute bottom-0 w-full h-16"></div>
+        <div className="w-60 mb-10 p-2 bg-red-600 flex flex-col items-center text-white">
+          <div className="w-54 h-48 overflow-hidden">
+            <img src={pic} alt="app-description" className="w-54 object-fit mb-2" />
+          </div>
+          <p className="font-bold text-3xl mb-2">point</p>
+          <p className="font-bold text-base">地球をぐるぐる回しながら世界で活躍するフェローを探せる！</p>
         </div>
-
         <div className="radial-progress text-red-600 mb-2" style={style}>
           {progress}%
         </div>
